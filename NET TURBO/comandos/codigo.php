b605235af426f0c2d66557958e13c1ab3cb477fd
NETTURBOBR <74615479+NETTURBOBR@users.noreply.github.com>
2022-05-05 10:41:29 -0400

    Update codigo.php

--- NET TURBO/comandos/codigo.php
+++ NET TURBO/comandos/codigo.php
@@ -2,6 +2,6 @@
 
 $tlg->sendMessage ([
 	'chat_id' => $tlg->ChatID (),
-	'text' => "<b>O código fonte desse bot está a venda, contate @Anjo_244 e tenha o seu proprio bot de sms.</b>\n\n<b>#resquisitos:</b> <code>php >=7.4, redis, sqlite3, Linux (ubuntu >=18)</code>\n\n VALOR: $300",
+	'text' => "<b>O código fonte desse bot está a venda, contate @NET_TURBO_VPSBR e tenha o seu proprio bot de sms.</b>\n\n<b>#resquisitos:</b> <code>php >=7.4, redis, sqlite3, Linux (ubuntu >=18)</code>\n\n VALOR: $300",
 	'parse_mode' => 'html'
 ]);
