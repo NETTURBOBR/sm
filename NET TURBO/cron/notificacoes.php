3e98c705487907ffc685850a1c7b2b0e08616be9
NETTURBOBR <74615479+NETTURBOBR@users.noreply.github.com>
2022-05-05 11:17:04 -0400

    Update notificacoes.php

--- NET TURBO/cron/notificacoes.php
+++ NET TURBO/cron/notificacoes.php
@@ -9,20 +9,20 @@ foreach ($bd_tlg->todosUsuarios () as $usuario){
 
 /*	$msg = @$tlg->sendMessage ([
 		'chat_id' => $usuario ['id_telegram'],
-		'text' => "🚀 <b>Gere Números Para Receber SMS no Seu Serviço Preferido, TikTok, Kwai, PicPay, Whatsapp, Telegram, BanQi...</b>\n\n💠 É Facíl, Apenas Recarregue Sua Conta Com o Comando /recarregar e Use o Saldo Para Comprar Números, Não Se Preocupe Você Só Paga Depois Que Recebe o Sms!\n\n🎃 Nosso Grupo De Consultas Gratis. https://t.me/MandrackSMSChecker",
+		'text' => "🚀 <b>Gere Números Para Receber SMS no Seu Serviço Preferido, TikTok, Kwai, PicPay, Whatsapp, Telegram, BanQi...</b>\n\n💠 É Facíl, Apenas Recarregue Sua Conta Com o Comando /recarregar e Use o Saldo Para Comprar Números, Não Se Preocupe Você Só Paga Depois Que Recebe o Sms!\n\n🎃 Nosso Grupo De Consultas Gratis. https://t.me/Recebsms",
 		'parse_mode' => 'html'
 	]);*/
 
 	$msg = $tlg->sendMessage ([
 	 	'chat_id' => $usuario ['id_telegram'],
 	 	'from_chat_id' => CHAT_ID_NOTIFICACAO,
-	 	'text' => "🚀 <b>Gere Números Para Receber SMS no Seu Serviço Preferido, TikTok, Kwai, PicPay, Whatsapp, Telegram, BanQi...</b>\n\n💠 É Facíl, Apenas Recarregue Sua Conta Com o Comando /recarregar e Use o Saldo Para Comprar Números, Não Se Preocupe Você Só Paga Depois Que Recebe o Sms!\n\n🎃 Nosso Grupo De Consultas Gratis. https://t.me/MandrackSMSChecker",
+	 	'text' => "🚀 <b>Gere Números Para Receber SMS no Seu Serviço Preferido, TikTok, Kwai, PicPay, Whatsapp, Telegram, BanQi...</b>\n\n💠 É Facíl, Apenas Recarregue Sua Conta Com o Comando /recarregar e Use o Saldo Para Comprar Números, Não Se Preocupe Você Só Paga Depois Que Recebe o Sms!\n\n🎃 Nosso Grupo De Consultas Gratis. https://t.me/Recebsms",
 		'parse_mode' => 'html'
 	 ]); 
 
 	/* $msg = @$tlg->sendMessage ([
 	 	'chat_id' => $usuario ['id_telegram'],
-	 	'text' => "✨ Use o comando /totaladicionados para saber a quantidade de usuários que você adicionou no nosso grupo @chatrecebersms\n\n<u>Adicionando ".MINIMO_ADICAO." usuários você ganha R\$".number_format (BONUS_ADICAO, 2)." de saldo no bot</u>",
+	 	'text' => "✨ Use o comando /totaladicionados para saber a quantidade de usuários que você adicionou no nosso grupo https://t.me/Recebsms\n\n<u>Adicionando ".MINIMO_ADICAO." usuários você ganha R\$".number_format (BONUS_ADICAO, 2)." de saldo no bot</u>",
 	 	'parse_mode' => 'html'
 	 ]);*/
 
