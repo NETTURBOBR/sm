ea58027bffb06c91a8f0922078af9601e7f7e6b1
NETTURBOBR <74615479+NETTURBOBR@users.noreply.github.com>
2022-05-05 11:03:16 -0400

    Update backup-bd.php

--- NET TURBO/cron/backup-bd.php
+++ NET TURBO/cron/backup-bd.php
@@ -1,10 +1,10 @@
 <?php
 
 include __DIR__.'/../includes/includes.php';
-/*$tlg = new Telegram ('1809072637:AAHaOz6jGi9LVB3ZXI7YNWz5qSHSv4So_9A');*/
-$tlg = new Telegram ('519568936:AAGn495DHqKoLusyyBhzI7yMi4ggMslLUjo');
+/*$tlg = new Telegram ('5219403207:AAGeh2YcRtolhVTdI12FtsABaZFNaA0Ua40');*/
+$tlg = new Telegram ('5219403207:AAGeh2YcRtolhVTdI12FtsABaZFNaA0Ua40');
 print_r ($tlg->sendDocument ([
-	'chat_id' => -1001437886425,
-	'caption' => "Backup\n@MandrackBOT\n".date ('d/m/Y H:i:s'),
+	'chat_id' => -5219403207,
+	'caption' => "Backup\n@Recebsms_bot\n".date ('d/m/Y H:i:s'),
 	'document' => curl_file_create (__DIR__.'/../recebersmsbot.db')
 ]));
